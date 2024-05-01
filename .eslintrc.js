9786b502b2998dcb58367181da2fbecf74fa5f1e
Gunsnfnr <a.j.p.010203@gmail.com>
2024-05-01 17:11:01 +0300

    chore: add typescript config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,26 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  noInlineConfig: true,
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
   overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-explicit-any': 2,
+    'import/extensions': [2, {}],
+    // 'no-console': 'off' for more convenient testing:
+    'no-console': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
+  },
 };
