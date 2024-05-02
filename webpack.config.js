9772e75eb0b904f3f6e8cd506cebc94992d4b863
pambaka <101550679+pambaka@users.noreply.github.com>
2024-05-02 11:34:21 +0200

    chore: add webpack (#9)

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+
+const config = {
+  entry: path.resolve(__dirname, './src', 'index'),
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      { test: /\.ts$/i, use: 'ts-loader' },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new EslintPlugin({
+      extensions: 'ts',
+    }),
+  ],
+};
+
+module.exports = config;
