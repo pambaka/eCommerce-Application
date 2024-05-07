694ba801670e978c30a67f3860c575569fe3c77d
pambaka <pambaka@ya.ru>
2024-05-07 23:01:27 +0200

    feat: add password validation

--- src/pages/const.ts
+++ src/pages/const.ts
@@ -4,3 +4,12 @@ export const FORM_TYPE = {
 } as const;
 
 export type FormType = typeof FORM_TYPE.login | typeof FORM_TYPE.registration;
+
+export const minLength = {
+  password: 8,
+};
+
+export const reUpperLetter: RegExp = /[A-Z]/g;
+export const reLowerLetter: RegExp = /[a-z]/g;
+export const reDigit: RegExp = /[0-9]/g;
+export const reLettersDigits: RegExp = /^[a-zA-Z0-9]/g;
--- src/pages/validate-password.ts
+++ src/pages/validate-password.ts
@@ -1,7 +1,39 @@
+import { minLength, reDigit, reLettersDigits, reLowerLetter, reUpperLetter } from './const';
+
+const message: { [key: string]: string } = {
+  upperCaseLetter: 'one uppercase letter (A-Z)',
+  lowerCaseLetter: 'one lowercase letter (a-z)',
+  digit: 'one digit (0-9)',
+  base: 'Include at least',
+};
+
 export default function validatePassword(password: string): string {
   let warning: string = '';
 
-  if (password === '') warning = 'password is required';
+  const passwordArr: string[] = password.split('');
+
+  if (password === '') {
+    warning = 'Password is required.';
+  } else if (!passwordArr.every((char) => char.match(reLettersDigits))) {
+    warning = 'Only english letters and digits are allowed.';
+  } else {
+    if (password.length < minLength.password) warning = `Minimum password length is ${minLength.password} symbols.`;
+
+    const warningMessages: string[] = [];
+
+    if (!passwordArr.some((char) => char.match(reUpperLetter))) {
+      warningMessages.push(message.upperCaseLetter);
+    }
+    if (!passwordArr.some((char) => char.match(reLowerLetter))) {
+      warningMessages.push(message.lowerCaseLetter);
+    }
+    if (!passwordArr.some((char) => char.match(reDigit))) {
+      warningMessages.push(message.digit);
+    }
+    if (warningMessages.length !== 0) {
+      warning += ` ${message.base} ${warningMessages.join(', ')}.`;
+    }
+  }
 
   return warning;
 }
