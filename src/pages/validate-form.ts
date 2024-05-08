1f9a5c62136a017d0617c3b36c97088a8fac9174
pambaka <pambaka@ya.ru>
2024-05-08 21:49:41 +0200

    feat: display error messages for login form validation issues

--- src/pages/validate-form.ts
+++ src/pages/validate-form.ts
@@ -2,21 +2,24 @@ import { CLASS_NAMES, DOM } from '../const';
 import { FORM_TYPE } from './const';
 import validateInput from './validate-input';
 
-export default function validateForm(event: Event) {
-  const input = event.target;
+export default function validateForm(event: Event): void {
+  const input: EventTarget | null = event.target;
 
   if (input instanceof HTMLInputElement) {
     // class name is supposed to be in format: FORM_TYPE[key] + __ + warning[FORM_TYPE[key]] key (from validate-input.ts)
-    const formType = input.classList[0].split('__')[0];
+    const formType: string = input.classList[0].split('__')[0];
 
     if (formType === FORM_TYPE.login) {
-      const button = DOM.elements[CLASS_NAMES.loginButton];
-
+      const button: HTMLElement = DOM.elements[CLASS_NAMES.loginButton];
       button.setAttribute('disabled', '');
 
       const warning = validateInput(formType, input);
 
-      console.log(warning);
+      const inputModule: HTMLElement | null = input.parentElement;
+      const inputName: string = input.classList[0].split('__')[1];
+      if (inputModule && inputName in warning) {
+        inputModule.setAttribute('warning', warning[inputName]);
+      }
 
       if (Object.values(warning).every((elem) => elem === '')) {
         const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input');
