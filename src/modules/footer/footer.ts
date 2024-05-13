b83ffeaa535644381f0d1b166a01eb297dc1f595
TMaltseva <bacardeonie@gmail.com>
2024-05-13 20:36:07 +0300

    fix: add component render to the footer

--- src/modules/footer/footer.ts
+++ src/modules/footer/footer.ts
@@ -1,46 +1,82 @@
 import BaseComponent from '../../components/base-component';
+import BaseTextComponent from '../../components/base-text-component';
 
 export default class Footer extends BaseComponent {
-  public container: HTMLElement;
-
   constructor() {
     super('footer', 'footer');
-    this.container = this.node;
     this.renderFooter();
   }
 
-  renderFooter() {
-    this.node.innerHTML = `
-      <div class="footer_contacts">
-        <h2 class="footer_title">Contact Us</h2>
-        <ul class="contacts_list">
-          <li class="contacts_item">
-            <p class="contacts_title">Email</p>
-            <a href="mailto:keep_calm_and_code@gmail.com" class="footer_text link">keep_calm_and_code@gmail.com</a>
-          </li>
-          <li class="contacts_item">
-            <p class="contacts_title">Phone number</p>
-            <a href="tel:+79995201234" class="footer_text">8-999-520-1234</a>
-          </li>
-          <li class="contacts_item">
-            <p class="contacts_title">Address</p>
-            <a class="footer_text link">Lehendakarianen, 5, Bilbo</a>
-          </li>
-        </ul>
-      </div>
-      <div class="footer_info">
-        <div class="footer_logo">
-          <img src="https://www.svgrepo.com/show/530291/leaves-2.svg" alt="logo" class="logo_img" width="50" height="50">
-          <h1 class="logo_text">KeepCalm</h1>
-        </div>
-        <p class="footer_text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel augue at arcu fermentum blandit. In hac habitasse platea dictumst. Pellentesque aliquam dui urna, quis fermentum massa posuere eu. Aliquam vel venenatis lectus, consequat semper risus. Vivamus libero dui, ornare vitae est pharetra, lacinia elementum ligula. Aliquam eget orci et sem posuere blandit eu ac neque. Donec bibendum venenatis ante, in varius velit dapibus at.</p>
-        <ul class="info_list">
-          <li class="info_item"><a href="" class="info_link"><img src="https://www.svgrepo.com/show/521711/instagram.svg" alt="inst" class="info_icon" width="30" height="30"></a></li>
-          <li class="info_item"><a href="" class="info_link"><img src="https://www.svgrepo.com/show/509923/facebook.svg" alt="facebook" class="info_icon" width="30" height="30"></a></li>
-          <li class="info_item"><a href="https://rs.school/" class="info_link"><img src="https://rs.school/assets/rs-logo-2XN05XgC.webp" alt="rs logo" class="info_icon" width="30" height="30"></a></li>
-        </ul>
-      </div>
-    `;
+  static addContactItem(parentNode: HTMLElement, titleText: string, href: string, linkText: string): void {
+    const item = new BaseComponent('li', 'contacts_item');
+    const title = new BaseTextComponent('p', 'contacts_title', titleText);
+    const link = new BaseComponent('a', 'footer_text');
+    link.node.classList.add('link');
+    link.node.setAttribute('href', href);
+    link.node.textContent = linkText;
+    item.node.appendChild(title.node);
+    item.node.appendChild(link.node);
+    parentNode.appendChild(item.node);
+  }
+
+  private renderFooter(): void {
+    const contactsDiv = new BaseComponent('div', 'footer_contacts');
+    const contactsTitle = new BaseTextComponent('h2', 'footer_title', 'Contact Us');
+    const contactsList = new BaseComponent('ul', 'contacts_list');
+
+    Footer.addContactItem(
+      contactsList.node,
+      'Email',
+      'mailto:keep_calm_and_code@gmail.com',
+      'keep_calm_and_code@gmail.com',
+    );
+    Footer.addContactItem(contactsList.node, 'Phone number', 'tel:+79995201234', '8-999-520-1234');
+    Footer.addContactItem(contactsList.node, 'Address', '#', 'Lehendakarianen, 5, Bilbo');
+
+    contactsDiv.node.appendChild(contactsTitle.node);
+    contactsDiv.node.appendChild(contactsList.node);
+
+    const infoDiv = new BaseComponent('div', 'footer_info');
+    const logoDiv = new BaseComponent('div', 'footer_logo');
+    const logoImg = new BaseComponent('img');
+    logoImg.node.setAttribute('src', 'https://www.svgrepo.com/show/530291/leaves-2.svg');
+    logoImg.node.setAttribute('alt', 'logo');
+    logoImg.node.setAttribute('width', '50');
+    logoImg.node.setAttribute('height', '50');
+    const logoText = new BaseTextComponent('h1', 'logo_text', 'KeepCalm');
+    const footerText = new BaseTextComponent(
+      'p',
+      'footer_text',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel augue at arcu fermentum blandit. In hac habitasse platea dictumst. Pellentesque aliquam dui urna, quis fermentum massa posuere eu. Aliquam vel venenatis lectus, consequat semper risus. Vivamus libero dui, ornare vitae est pharetra, lacinia elementum ligula. Aliquam eget orci et sem posuere blandit eu ac neque. Donec bibendum venenatis ante, in varius velit dapibus at.',
+    );
+    const infoList = new BaseComponent('ul', 'info_list');
+
+    // Adding social icons
+    const addSocialIcon = (href: string, src: string, alt: string) => {
+      const item = new BaseComponent('li', 'info_item');
+      const link = new BaseComponent('a', 'info_link');
+      link.node.setAttribute('href', href);
+      const icon = new BaseComponent('img', 'info_icon');
+      icon.node.setAttribute('src', src);
+      icon.node.setAttribute('alt', alt);
+      icon.node.setAttribute('width', '30');
+      icon.node.setAttribute('height', '30');
+      link.node.appendChild(icon.node);
+      item.node.appendChild(link.node);
+      infoList.node.appendChild(item.node);
+    };
+
+    addSocialIcon('#', 'https://www.svgrepo.com/show/521711/instagram.svg', 'inst');
+    addSocialIcon('#', 'https://www.svgrepo.com/show/509923/facebook.svg', 'facebook');
+    addSocialIcon('https://rs.school/', 'https://rs.school/assets/rs-logo-2XN05XgC.webp', 'rs logo');
+
+    logoDiv.node.appendChild(logoImg.node);
+    logoDiv.node.appendChild(logoText.node);
+    infoDiv.node.appendChild(logoDiv.node);
+    infoDiv.node.appendChild(footerText.node);
+    infoDiv.node.appendChild(infoList.node);
+
+    this.node.appendChild(contactsDiv.node);
+    this.node.appendChild(infoDiv.node);
   }
 }
