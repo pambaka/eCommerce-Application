03d64c8f0547c5d664293275fa79a384c19b6b91
TMaltseva <bacardeonie@gmail.com>
2024-05-15 00:26:16 +0300

    fix: change nav component with link component inside

--- src/components/navigation-component.ts
+++ src/components/navigation-component.ts
@@ -1,12 +1,10 @@
 import BaseComponent from './base-component';
+import BaseLinkComponent from './base-link-component';
 
 export default class NavItemComponent extends BaseComponent {
   constructor(href: string, text: string) {
     super('li', 'nav_item');
-    const a = document.createElement('a');
-    a.setAttribute('href', href);
-    a.textContent = text;
-    a.classList.add('nav_link');
-    this.node.appendChild(a);
+    const a = new BaseLinkComponent(href, 'nav_link', text);
+    this.node.appendChild(a.node);
   }
 }
