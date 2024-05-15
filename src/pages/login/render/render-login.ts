f1a299b3c452887d3113323dc9fb7b15c6b1fa2c
pambaka <101550679+pambaka@users.noreply.github.com>
2024-05-15 13:33:44 +0200

    feat: RSS-ECOMM-2_08 add a button to navigate to the registration page (#37)
    (login page)

--- src/pages/login/render/render-login.ts
+++ src/pages/login/render/render-login.ts
@@ -1,32 +1,16 @@
 import '../../../styles/login-page.scss';
+import Header from '../../../modules/header-form/header';
+import renderForm from './render-form';
 import BaseComponent from '../../../components/base-component';
-import ButtonComponent from '../../../components/button-component';
-import inputModule from '../../../modules/input-module';
-import { CLASS_NAMES, DOM } from '../../../const';
-import LabelComponent from '../../../components/label-component';
-import togglePasswordVisibility from '../logic/toggle-password-visibility';
-import handleLoginRequest from '../logic/handle-login-request';
 
 export default function renderLoginPage(): void {
-  const loginForm = new BaseComponent<HTMLFormElement>('form', 'login-form');
+  document.body.innerHTML = '';
 
-  const inputLogin: HTMLDivElement = inputModule(CLASS_NAMES.loginLogin, 'email', 'Login (e-mail) *');
-  const inputPassword: HTMLDivElement = inputModule(CLASS_NAMES.loginPassword, 'password', 'Password *');
+  const header = new Header();
+  header.renderUserNavPanel(['register']);
 
-  const checkboxLabel = new LabelComponent('Show password');
-  checkboxLabel.node.classList.add('password-checkbox__label');
+  const main = new BaseComponent('main', 'main-login');
+  renderForm(main.node);
 
-  const passwordCheckbox = new BaseComponent<HTMLInputElement>('input');
-  passwordCheckbox.node.type = 'checkbox';
-  passwordCheckbox.node.textContent = 'Show password';
-  passwordCheckbox.node.addEventListener('click', togglePasswordVisibility);
-
-  checkboxLabel.node.prepend(passwordCheckbox.node);
-  inputPassword.append(checkboxLabel.node);
-
-  const button = new ButtonComponent(CLASS_NAMES.loginButton, handleLoginRequest, 'login', true);
-  DOM.add(CLASS_NAMES.loginButton, button.node);
-
-  loginForm.node.append(inputLogin, inputPassword, button.node);
-  document.body.append(loginForm.node);
+  document.body.append(header.node, main.node);
 }
