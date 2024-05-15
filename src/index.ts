2c81d3096b9b949af050c1a8febc7f3cd9d30936
TMaltseva <bacardeonie@gmail.com>
2024-05-15 14:35:11 +0300

    feat: add base routes to main and error pages in App class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,54 @@
 import './style.scss';
+
 import Header from './modules/header/header';
 import MainSection from './pages/main/render/render-main';
+import ErrorSection from './pages/error/render/render-error-page';
 import Footer from './modules/footer/footer';
+import Router from './services/router';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const header = new Header();
-  const mainSection = new MainSection();
-  const footer = new Footer();
+class App {
+  private header: Header;
 
-  document.body.append(header.node, mainSection.node, footer.node);
-});
+  private mainSection: MainSection;
+
+  private errorSection: ErrorSection;
+
+  private footer: Footer;
+
+  constructor() {
+    this.header = new Header();
+    this.mainSection = new MainSection();
+    this.errorSection = new ErrorSection();
+    this.footer = new Footer();
+  }
+
+  init() {
+    document.addEventListener('DOMContentLoaded', () => {
+      window.addEventListener('hashchange', this.render.bind(this));
+      this.render();
+    });
+  }
+
+  render() {
+    const route = window.location.hash || Router.pages.main;
+
+    document.body.innerHTML = '';
+    document.body.prepend(this.header.node);
+
+    switch (route) {
+      case Router.pages.main:
+        document.body.append(this.mainSection.node);
+        break;
+      case Router.pages.notFound:
+        document.body.append(this.errorSection.node);
+        break;
+      default:
+        document.body.append(this.errorSection.node);
+    }
+
+    document.body.append(this.footer.node);
+  }
+}
+
+const app = new App();
+app.init();
