1eb7e11e18faa77671d2605536ed02a120d1a6a9
Tommy Maltseva <134083156+TMaltseva@users.noreply.github.com>
2024-05-15 16:36:25 +0400

    Feat/rss ecomm 2 18 (#39)
    * feat: add base routes to main and error pages in App class
    
    * feat: add route to main from error page and fix error style import
    
    * fix: instead of clearing the body, add creation this.contentNode for dynamic content, which is cleared and updated on every render
    
    * fix: delete vscode folder
    
    * fix: document.body.append content
    
    * fix: move App class to the separate folder app
    
    * fix: delete vscode folder

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import './style.scss';
-import Header from './modules/header/header';
-import MainSection from './pages/main/render/render-main';
-import Footer from './modules/footer/footer';
+import App from './app/app';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const header = new Header();
-  const mainSection = new MainSection();
-  const footer = new Footer();
-
-  document.body.append(header.node, mainSection.node, footer.node);
-});
+const app = new App();
+app.init();
