1f2225dd3336750abd20544978540ee86c25ddc1
TMaltseva <bacardeonie@gmail.com>
2024-05-17 22:38:11 +0300

    feat: add constant routes from Router

--- src/modules/header/header.ts
+++ src/modules/header/header.ts
@@ -6,6 +6,7 @@ import BaseLinkComponent from '../../components/base-link-component';
 import BurgerMenuButton from '../../components/burger-menu-component';
 import BurgerMenuIcon from '../../components/burger-menu-icon';
 import SideMenuComponent from '../../components/side-menu-component';
+import Router from '../../services/router';
 
 export default class Header extends BaseComponent {
   private burgerMenuIcon: BurgerMenuButton | null = null;
@@ -25,7 +26,7 @@ export default class Header extends BaseComponent {
     ul.node.classList.add('nav_header');
 
     const logoItem = new BaseComponent('li', 'nav_item');
-    const logoLink = new BaseLinkComponent('#main', 'header_logo', '');
+    const logoLink = new BaseLinkComponent(Router.pages.main, 'header_logo', '');
 
     const logoImage = new BaseImageComponent('logo_image', 'https://www.svgrepo.com/show/530291/leaves-2.svg', 'logo');
     const logoText = new BaseTextComponent('h1', 'logo_text', 'KeepCalm');
@@ -36,16 +37,16 @@ export default class Header extends BaseComponent {
     ul.node.appendChild(logoItem.node);
 
     // Navigation items
-    ul.node.appendChild(new NavItemComponent('#main', 'Home').node);
-    ul.node.appendChild(new NavItemComponent('#about', 'About').node);
-    ul.node.appendChild(new NavItemComponent('#catalog', 'Catalog').node);
-    ul.node.appendChild(new NavItemComponent('#login', 'Log in').node);
-    ul.node.appendChild(new NavItemComponent('#signup', 'Sign up').node);
+    ul.node.appendChild(new NavItemComponent(Router.pages.main, 'Home').node);
+    ul.node.appendChild(new NavItemComponent(Router.pages.about, 'About').node);
+    ul.node.appendChild(new NavItemComponent(Router.pages.catalog, 'Catalog').node);
+    ul.node.appendChild(new NavItemComponent(Router.pages.login, 'Log in').node);
+    ul.node.appendChild(new NavItemComponent(Router.pages.signup, 'Sign up').node);
 
     // Cart icon
     const cartItem = new BaseComponent('li', 'nav_item');
     cartItem.node.classList.add('nav_item_cart');
-    const cartLink = new BaseLinkComponent('#cart', 'nav_link', '');
+    const cartLink = new BaseLinkComponent(Router.pages.cart, 'nav_link', '');
     const cartIcon = new BaseImageComponent('cart_icon', 'https://www.svgrepo.com/show/529445/cart-3.svg', 'cart');
     cartIcon.node.classList.add('cart');
     cartLink.node.appendChild(cartIcon.node);
