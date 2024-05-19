585bdb61f4bf5a669ddf70168e9493ed638e68b0
Gunsnfnr <a.j.p.010203@gmail.com>
2024-05-19 13:18:53 +0300

    feat: integrate extended registration with eCommerce

--- src/pages/registration/logic/handle-submit.ts
+++ src/pages/registration/logic/handle-submit.ts
@@ -1,5 +1,6 @@
 import signUpCustomer from '../../../api/sign-up-customer';
 import { CLASS_NAMES, ID_NAMES } from '../../../const';
+import { Address } from '../../../types/index';
 import assertNonNullable from '../../../utils/assert-non-nullable';
 
 export default function handleSubmitRegistration(event: Event) {
@@ -15,21 +16,56 @@ export default function handleSubmitRegistration(event: Event) {
   const cityShipping = assertNonNullable<HTMLInputElement>(`.registration__address .${CLASS_NAMES.city}`);
   const isDefaultShipping = assertNonNullable<HTMLInputElement>(`#${ID_NAMES.defaultShipping}`);
 
-  // const countryBilling = assertNonNullable<HTMLSelectElement>(
-  //   `.registration__shipping-address .${CLASS_NAMES.country}`,
-  // );
-  signUpCustomer(
-    email.value,
-    password.value,
-    firstName.value,
-    lastName.value,
-    birthDate.value,
-    {
-      country: countryShipping.value,
-      postalCode: postalCodeShipping.value,
-      streetName: streetShipping.value,
-      city: cityShipping.value,
-    },
-    isDefaultShipping.checked,
-  );
+  const isUsedforBilling = assertNonNullable<HTMLInputElement>(`#${ID_NAMES.useForBilling}`);
+
+  const shippingAddress: Address = {
+    country: countryShipping.value,
+    postalCode: postalCodeShipping.value,
+    streetName: streetShipping.value,
+    city: cityShipping.value,
+  };
+
+  if (isUsedforBilling.checked) {
+    signUpCustomer(
+      email.value,
+      password.value,
+      firstName.value,
+      lastName.value,
+      birthDate.value,
+      shippingAddress,
+      isDefaultShipping.checked,
+      shippingAddress,
+      false,
+    );
+  } else {
+    const countryBilling = assertNonNullable<HTMLSelectElement>(
+      `.registration__shipping-address .${CLASS_NAMES.country}`,
+    );
+    const postalCodeBilling = assertNonNullable<HTMLInputElement>(
+      `.registration__shipping-address .${CLASS_NAMES.postalCode}`,
+    );
+    const streetBilling = assertNonNullable<HTMLInputElement>(`.registration__shipping-address .${CLASS_NAMES.street}`);
+    const cityBilling = assertNonNullable<HTMLInputElement>(`.registration__shipping-address .${CLASS_NAMES.city}`);
+
+    const isDefaultBilling = assertNonNullable<HTMLInputElement>(`#${ID_NAMES.defaultBilling}`);
+
+    const billingAddress: Address = {
+      country: countryBilling.value,
+      postalCode: postalCodeBilling.value,
+      streetName: streetBilling.value,
+      city: cityBilling.value,
+    };
+
+    signUpCustomer(
+      email.value,
+      password.value,
+      firstName.value,
+      lastName.value,
+      birthDate.value,
+      shippingAddress,
+      isDefaultShipping.checked,
+      billingAddress,
+      isDefaultBilling.checked,
+    );
+  }
 }
