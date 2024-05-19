47b56fb1a163cd837d11936c33f610d8e0c9747c
Gunsnfnr <a.j.p.010203@gmail.com>
2024-05-19 12:27:47 +0300

    feat: add validation form for adding/removing billing block

--- src/pages/registration/logic/handle-submit.ts
+++ src/pages/registration/logic/handle-submit.ts
@@ -1,43 +1,35 @@
 import signUpCustomer from '../../../api/sign-up-customer';
 import { CLASS_NAMES, ID_NAMES } from '../../../const';
+import assertNonNullable from '../../../utils/assert-non-nullable';
 
 export default function handleSubmitRegistration(event: Event) {
   event.preventDefault();
-  const email = document.querySelector(`.${CLASS_NAMES.emailInput}`);
-  const password = document.querySelector(`.${CLASS_NAMES.passwordInput}`);
-  const firstName = document.querySelector(`.${CLASS_NAMES.firstName}`);
-  const lastName = document.querySelector(`.${CLASS_NAMES.lastName}`);
-  const birthDate = document.querySelector(`.${CLASS_NAMES.birthDate}`);
-  const countryShipping = document.querySelector(`.registration__address .${CLASS_NAMES.country}`);
-  const postalCodeShipping = document.querySelector(`.registration__address .${CLASS_NAMES.postalCode}`);
-  const streetShipping = document.querySelector(`.registration__address .${CLASS_NAMES.street}`);
-  const cityShipping = document.querySelector(`.registration__address .${CLASS_NAMES.city}`);
-  const isDefaultShipping = document.getElementById(ID_NAMES.defaultShipping);
-  if (
-    email instanceof HTMLInputElement &&
-    password instanceof HTMLInputElement &&
-    firstName instanceof HTMLInputElement &&
-    lastName instanceof HTMLInputElement &&
-    birthDate instanceof HTMLInputElement &&
-    countryShipping instanceof HTMLSelectElement &&
-    postalCodeShipping instanceof HTMLInputElement &&
-    streetShipping instanceof HTMLInputElement &&
-    cityShipping instanceof HTMLInputElement &&
-    isDefaultShipping instanceof HTMLInputElement
-  ) {
-    signUpCustomer(
-      email.value,
-      password.value,
-      firstName.value,
-      lastName.value,
-      birthDate.value,
-      {
-        country: countryShipping.value,
-        postalCode: postalCodeShipping.value,
-        streetName: streetShipping.value,
-        city: cityShipping.value,
-      },
-      isDefaultShipping.checked,
-    );
-  }
+  const email = assertNonNullable<HTMLInputElement>(`.${CLASS_NAMES.emailInput}`);
+  const password = assertNonNullable<HTMLInputElement>(`.${CLASS_NAMES.passwordInput}`);
+  const firstName = assertNonNullable<HTMLInputElement>(`.${CLASS_NAMES.firstName}`);
+  const lastName = assertNonNullable<HTMLInputElement>(`.${CLASS_NAMES.lastName}`);
+  const birthDate = assertNonNullable<HTMLInputElement>(`.${CLASS_NAMES.birthDate}`);
+  const countryShipping = assertNonNullable<HTMLSelectElement>(`.registration__address .${CLASS_NAMES.country}`);
+  const postalCodeShipping = assertNonNullable<HTMLInputElement>(`.registration__address .${CLASS_NAMES.postalCode}`);
+  const streetShipping = assertNonNullable<HTMLInputElement>(`.registration__address .${CLASS_NAMES.street}`);
+  const cityShipping = assertNonNullable<HTMLInputElement>(`.registration__address .${CLASS_NAMES.city}`);
+  const isDefaultShipping = assertNonNullable<HTMLInputElement>(`#${ID_NAMES.defaultShipping}`);
+
+  // const countryBilling = assertNonNullable<HTMLSelectElement>(
+  //   `.registration__shipping-address .${CLASS_NAMES.country}`,
+  // );
+  signUpCustomer(
+    email.value,
+    password.value,
+    firstName.value,
+    lastName.value,
+    birthDate.value,
+    {
+      country: countryShipping.value,
+      postalCode: postalCodeShipping.value,
+      streetName: streetShipping.value,
+      city: cityShipping.value,
+    },
+    isDefaultShipping.checked,
+  );
 }
--- src/pages/registration/render/toggle-billing-address-block.ts
+++ src/pages/registration/render/toggle-billing-address-block.ts
@@ -1,11 +1,19 @@
+import { CLASS_NAMES, DOM } from '../../../const';
+import assertNonNullable from '../../../utils/assert-non-nullable';
 import renderBillingAddressBlock from './render-billing-address-block';
 
 export default function toggleBillingAddressBlock(event: Event) {
+  const regSubmitButton = DOM.elements[CLASS_NAMES.registrationButton];
   if (event.target instanceof HTMLInputElement && event.target.checked) {
     console.log('checked');
     const billingAddressBlock = document.querySelector('.registration__shipping-address');
     if (billingAddressBlock) billingAddressBlock.remove();
+    regSubmitButton.removeAttribute('disabled');
+    const keyUpEvent = new KeyboardEvent('keyup');
+    const nameInput = assertNonNullable<HTMLInputElement>('.registration__first-name');
+    nameInput.dispatchEvent(keyUpEvent);
   } else {
     renderBillingAddressBlock();
+    regSubmitButton.setAttribute('disabled', 'true');
   }
 }
