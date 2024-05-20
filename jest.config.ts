b64869ae99fe9e52ee77c9a12f1b9b5d8a1adca0
pambaka <101550679+pambaka@users.noreply.github.com>
2024-05-20 15:27:25 +0200

    chore: add renderLoginPage and signInCustomer functions tests (#52)

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,25 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-      '\\.(scss|css)$': '<rootDir>/src/styleMock.ts',
-      '\\.(png|svg)$': '<rootDir>/src/assetsMock.ts',
-      '^@src/(.*)$': '<rootDir>/src/$1',
-      '^@components/(.*)$': '<rootDir>/src/components/$1',
-      '^@pages/(.*)$': '<rootDir>/src/pages/$1',
-      '^@services/(.*)$': '<rootDir>/src/services/$1',
-      '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
-      '^@assets/(.*)$': '<rootDir>/src/assets/$1',
-    },
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    testMatch: ['**/__tests__/**/*.(ts|tsx|js)'],
-    collectCoverageFrom: ['src/**/*.(ts|tsx)'],
-  };
-  
-  export {};
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: {
+    //   '^@/(.*)$': '<rootDir>/src/$1',
+    //   // '\\.(scss|css)$': '<rootDir>/src/styleMock.ts',
+    '\\.(scss)$': '<rootDir>/__tests__/mocks/styleMock.ts',
+    // '\\.(png|svg)$': '<rootDir>/src/assetsMock.ts',
+    '\\.(png|svg)$': '<rootDir>/__tests__/mocks/assetsMock.ts',
+    //   '^@src/(.*)$': '<rootDir>/src/$1',
+    //   '^@components/(.*)$': '<rootDir>/src/components/$1',
+    //   '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    //   '^@services/(.*)$': '<rootDir>/src/services/$1',
+    //   '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+    //   '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx|js)'],
+  collectCoverageFrom: ['src/**/*.(ts|tsx)'],
+};
+
+export {};
