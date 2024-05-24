2a0e55d56f6bc02a8aa1ab48687df0059aee7ea3
pambaka <pambaka@ya.ru>
2024-05-24 13:47:53 +0200

    feat: display prices on the product card in the catalog

--- src/modules/product-card/product-card.ts
+++ src/modules/product-card/product-card.ts
@@ -1,24 +1,49 @@
 import './product-card.scss';
 import BaseComponent from '../../components/base-component';
 import BaseTextComponent from '../../components/base-text-component';
+import { CardPrice } from '../../types/index';
 
 export default class ProductCard extends BaseComponent {
-  title;
-
-  image: HTMLElement;
-
-  description;
-
-  constructor(title: string, imgUrl: string, description: string) {
+  constructor(title: string, imageUrl: string, description: string, price: CardPrice) {
     super('article', 'product-card');
 
-    this.title = new BaseTextComponent('h3', 'card__title', title);
+    this.addTitle(title);
+    this.addImage(imageUrl);
+    this.addDescription(description);
+    this.addPrices(price);
+  }
 
-    this.image = document.createElement('div');
-    this.image.style.backgroundImage = `url(${imgUrl})`;
+  private addTitle(title: string) {
+    const cardTitle = new BaseTextComponent('h3', 'card__title', title);
 
-    this.description = new BaseTextComponent('p', 'card__description', description);
+    this.node.append(cardTitle.node);
+  }
 
-    this.node.append(this.title.node, this.image, this.description.node);
+  private addImage(imageUrl: string) {
+    const image = new BaseComponent('div', 'card__image');
+    image.node.style.backgroundImage = `url(${imageUrl})`;
+
+    this.node.append(image.node);
+  }
+
+  private addDescription(description: string) {
+    const cardDescription = new BaseTextComponent('p', 'card__description', description);
+
+    this.node.append(cardDescription.node);
+  }
+
+  private addPrices(price: CardPrice) {
+    const prices = new BaseComponent('div', 'card__prices');
+    const priceWrapper = new BaseTextComponent('p', 'price--regular', `€ ${String(price.regular)}`);
+    prices.node.append(priceWrapper.node);
+
+    if (price.discounted) {
+      priceWrapper.node.classList.add('price--old');
+
+      const discountedPriceWrapper = new BaseTextComponent('p', 'price--discount', `€ ${String(price.discounted)}`);
+      prices.node.append(discountedPriceWrapper.node);
+    }
+
+    this.node.append(prices.node);
   }
 }
--- src/pages/catalog/render/render-catalog.ts
+++ src/pages/catalog/render/render-catalog.ts
@@ -1,7 +1,7 @@
 import './catalog.scss';
 import BaseComponent from '../../../components/base-component';
 import useToken from '../../../services/use-token';
-import { Product } from '../../../types/index';
+import { CardPrice, Product } from '../../../types/index';
 import getProducts from '../../../api/get-products';
 import ProductCard from '../../../modules/product-card/product-card';
 import BaseTextComponent from '../../../components/base-text-component';
@@ -19,10 +19,18 @@ export default async function renderCatalog(): Promise<HTMLElement> {
         const currentProduct = products[i].masterData.current;
 
         const title: string = currentProduct.name['en-US'];
-        const description: string = currentProduct.description?.['en-US'] ?? 'no description available';
+
+        const description: string = currentProduct.description?.['en-US'] ?? '';
+
         const imageUrl: string = currentProduct.masterVariant.images[0]?.url ?? '';
 
-        const card = new ProductCard(title, imageUrl, description);
+        const price: CardPrice = { regular: undefined, discounted: undefined };
+        price.regular = currentProduct.masterVariant.prices[0]?.value.centAmount ?? 0;
+        price.regular /= 100;
+        const discounted = currentProduct.masterVariant.prices[0]?.discounted;
+        if (discounted) price.discounted = discounted.value.centAmount / 100;
+
+        const card = new ProductCard(title, imageUrl, description, price);
 
         wrapper.node.append(card.node);
       }
