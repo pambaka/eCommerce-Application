134adae9341f73db4b22b6e0d1f429ecc63175a5
TMaltseva <bacardeonie@gmail.com>
2024-05-25 12:28:36 +0300

    feat: add TOKEN_STORAGE_KEY to api consts

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,5 +1,6 @@
 export const region = 'eu-central-1.aws';
 export const storeKey = '';
+export const TOKEN_STORAGE_KEY = 'customerAccessToken';
 
 export const ERROR_MESSAGE = {
   notFound: 'Customer account with the given credentials not found.',
