d0f1ec755a99d7ad5c24fff15e0fb03ea00e8722
pambaka <pambaka@ya.ru>
2024-05-26 02:02:04 +0200

    feat: redirect to the product page when clicking on the product card

--- src/pages/catalog/render/render-catalog.ts
+++ src/pages/catalog/render/render-catalog.ts
@@ -5,6 +5,7 @@ import { CardPrice, Product } from '../../../types/index';
 import getProducts from '../../../api/get-products';
 import ProductCard from '../../../modules/product-card/product-card';
 import BaseTextComponent from '../../../components/base-text-component';
+import showProduct from '../logic/show-product';
 
 export default async function renderCatalog(): Promise<HTMLElement> {
   const wrapper = new BaseComponent('div', 'catalog-wrapper');
@@ -31,6 +32,8 @@ export default async function renderCatalog(): Promise<HTMLElement> {
         if (discounted) price.discounted = discounted.value.centAmount / 100;
 
         const card = new ProductCard(title, imageUrl, description, price);
+        card.node.setAttribute('key', products[i].key);
+        card.node.addEventListener('click', showProduct);
 
         wrapper.node.append(card.node);
       }
