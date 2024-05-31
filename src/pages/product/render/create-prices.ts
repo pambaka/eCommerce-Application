4639590838b610e141a17a2287b075bb1cbc0f95
Artyom P <126002661+Gunsnfnr@users.noreply.github.com>
2024-05-31 09:17:07 +0300

    feat: RSS-ECOMM-3_11 - Display Product Price and Sale Price (#83)
    display prices

---
+++ src/pages/product/render/create-prices.ts
@@ -0,0 +1,20 @@
+import BaseTextComponent from '../../../components/base-text-component';
+import { Product } from '../../../types/index';
+
+export default function createPrices(parentElement: HTMLElement, product: Product): void {
+  if (product.masterData.current.masterVariant.prices[0].discounted) {
+    const oldPrice = new BaseTextComponent(
+      'span',
+      'old-price',
+      `€ ${product.masterData.current.masterVariant.prices[0].discounted.value.centAmount / 100}`,
+    );
+    parentElement.append(oldPrice.node);
+  }
+
+  const price = new BaseTextComponent(
+    'span',
+    'price',
+    `€ ${product.masterData.current.masterVariant.prices[0].value.centAmount / 100}`,
+  );
+  parentElement.append(price.node);
+}
