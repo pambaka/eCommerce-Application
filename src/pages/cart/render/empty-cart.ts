2ff83436e92de7c64adae3177be0cdf2c425668c
Gunsnfnr <a.j.p.010203@gmail.com>
2024-06-10 10:19:20 +0300

    refactor: change button to anchor

--- src/pages/cart/render/empty-cart.ts
+++ src/pages/cart/render/empty-cart.ts
@@ -1,20 +1,13 @@
 import BaseImageComponent from '../../../components/base-image-component';
 import BaseTextComponent from '../../../components/base-text-component';
-import ButtonComponent from '../../../components/button-component';
 import Router from '../../../services/router';
 import emptyCartImage from '../../../assets/empty-cart.png';
+import BaseLinkComponent from '../../../components/base-link-component';
 
 export default function renderEmptyCart(parentElement: HTMLElement): void {
   const emptyImage = new BaseImageComponent('empty-image', emptyCartImage, 'Empty cart');
   const cartIsEmpty = new BaseTextComponent('h4', 'cart-is-empty', "Your cart is empty, but\xa0it's\xa0fixable\xa00_o");
-  const catalogButton = new ButtonComponent(
-    'catalog',
-    () => {
-      window.location.href = Router.pages.catalog;
-    },
-    'Open Catalog',
-    false,
-  );
+  const catalogButton = new BaseLinkComponent(Router.pages.catalog, 'catalog-link', 'OPEN CATALOG');
 
-  parentElement.append(emptyImage.node, emptyImage.node, cartIsEmpty.node, catalogButton.node);
+  parentElement.append(emptyImage.node, cartIsEmpty.node, catalogButton.node);
 }
