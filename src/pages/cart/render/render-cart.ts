3c20b55c6e94f2d17f6b003d25f736ce17ed66c6
Artyom P <126002661+Gunsnfnr@users.noreply.github.com>
2024-06-10 18:12:57 +0300

    feat: RSS-ECOMM-4_14, 15  - Display Message for Empty Shopping Cart and Catalog Link (#104)
    RSS-ECOMM-4_14: Display Message for Empty Shopping Cart
    RSS-ECOMM-4_15: Add Catalog Link to Empty Cart Message

--- src/pages/cart/render/render-cart.ts
+++ src/pages/cart/render/render-cart.ts
@@ -1,6 +1,9 @@
 import BaseComponent from '../../../components/base-component';
+import renderEmptyCart from './empty-cart';
+import './cart.scss';
 
 export default function renderCart(): HTMLElement {
   const cartWrapper = new BaseComponent('div', 'cart-wrapper');
+  renderEmptyCart(cartWrapper.node);
   return cartWrapper.node;
 }
