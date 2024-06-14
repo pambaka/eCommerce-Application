7c64543e8d4b12e35687f73efcbbfcb8cf923671
Artyom P <126002661+Gunsnfnr@users.noreply.github.com>
2024-06-14 13:48:39 +0300

    feat: RSS-ECOMM-4_12: Implement "Remove from Cart" Button (#117)
    * feat: implement Remove From Cart button
    
    * fix: use actual price for subtotal (for discounted products)
    
    * refactor: change style name

--- src/pages/cart/render/render-products-in-cart.ts
+++ src/pages/cart/render/render-products-in-cart.ts
@@ -1,29 +1,26 @@
 import BaseComponent from '../../../components/base-component';
 import BaseImageComponent from '../../../components/base-image-component';
 import BaseTextComponent from '../../../components/base-text-component';
+import CardCartButton from '../../../modules/card-cart-button/card-cart-button';
 import { Cart } from '../../../types/cart';
 import LANGUAGE from '../../../types/const';
 import handleQuantityChange from '../logic/handle-quantity-change';
 import limitInputValue from '../logic/limit-input-value';
+import removeProductFromCart from '../logic/remove-product-from-cart';
+import placeholderImage from '../../../assets/no_image.png';
+import restrictKeys from '../logic/restrict-keys';
+import getActualPrice from '../logic/get-actual-price';
 
 export default function renderProductsInCart(activeCart: Cart, parentElement: HTMLElement) {
   activeCart.lineItems.forEach((cartItem) => {
     const productWrapper = new BaseComponent('div', 'cart__product');
-    const productImage = new BaseImageComponent(
-      'cart__product-image',
-      cartItem.variant.images[0].url,
-      cartItem.name[LANGUAGE],
-    );
+    const imageSrc = cartItem.variant.images[0]?.url ? cartItem.variant.images[0].url : placeholderImage;
+    const productImage = new BaseImageComponent('cart__product-image', imageSrc, cartItem.name[LANGUAGE]);
 
     const productInfoWrapper = new BaseComponent('div', 'cart__product-info-wrapper');
     const productTitle = new BaseTextComponent('p', 'cart__product-name', cartItem.name[LANGUAGE]);
 
-    let productPriceValue;
-    if (cartItem.variant.prices[0].discounted) {
-      productPriceValue = cartItem.variant.prices[0].discounted.value.centAmount;
-    } else {
-      productPriceValue = cartItem.variant.prices[0].value.centAmount;
-    }
+    const productPriceValue = getActualPrice(cartItem);
 
     const priceInEuro = productPriceValue / 100;
 
@@ -44,26 +41,25 @@ export default function renderProductsInCart(activeCart: Cart, parentElement: HT
         event.target.blur();
       }
     });
-    productQuantityInput.node.addEventListener('keydown', (event) => {
-      // restrict input dot, plus, minus
-      if (
-        event.keyCode === 190 ||
-        event.keyCode === 191 ||
-        event.keyCode === 107 ||
-        event.keyCode === 187 ||
-        event.keyCode === 109 ||
-        event.keyCode === 189
-      ) {
-        event.preventDefault();
-      }
-    });
+    productQuantityInput.node.addEventListener('keydown', restrictKeys);
     productQuantityInput.node.value = String(cartItem.quantity);
     productQuantityContainer.node.append(productQuantity.node, productQuantityInput.node);
 
-    productInfoWrapper.node.append(productTitle.node, productPrice.node, productQuantityContainer.node);
+    const removeBtn = new CardCartButton(
+      'cart__remove-btn',
+      (event) => {
+        removeProductFromCart(event, cartItem);
+      },
+      'Remove',
+      false,
+    );
+    removeBtn.node.setAttribute('title', 'Remove from cart');
+    removeBtn.node.classList.add('remove-from-cart-button');
+
+    productInfoWrapper.node.append(productTitle.node, productPrice.node, productQuantityContainer.node, removeBtn.node);
 
     const productPriceWraper = new BaseComponent('div', 'cart__product-price-wrapper');
-    const productPriceTotal = new BaseTextComponent('p', 'cart__product-price-total', '€\xa0');
+    const productPriceTotal = new BaseTextComponent('p', 'cart__product-price-subtotal', '€\xa0');
 
     const sum = (cartItem.quantity * productPriceValue) / 100;
 
