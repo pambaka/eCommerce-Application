9e6f96d1508bed85597fa0383f30422e434388ad
TMaltseva <bacardeonie@gmail.com>
2024-06-18 00:37:49 +0300

    fix: add personal members descriptions and pics

--- src/pages/about/render/members-information.ts
+++ src/pages/about/render/members-information.ts
@@ -1,3 +1,6 @@
+import Artem from '../../../assets/member-images/Artem.png';
+import Toma from '../../../assets/member-images/Toma.png';
+
 const teamMembers = [
   {
     imgSrc:
@@ -6,7 +9,7 @@ const teamMembers = [
     name: 'Tanya Telegina',
     role: 'Lead Developer',
     bio: [
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'MIPT graduate, former engineer in a system integrator company (worked with network equipment), future frontend developer :) SVG and chess lover',
       '',
       '<b>Contribution to the project:</b>',
       ' - GitHub repository administration',
@@ -18,13 +21,12 @@ const teamMembers = [
     github: 'https://github.com/pambaka',
   },
   {
-    imgSrc:
-      'https://s3-alpha-sig.figma.com/img/f621/bdda/75dfbf7614cbd47aad1ee78575594fcd?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IN0Ng2IGGXOv39Dsf52TLmmTL81K4A2Us17uyKDYBPRd6WwQUfDmjbXYnKjeUZ2fvJDDml32f93luVaXb1yOfZcBe-BckW9HmtMJPqNdDn81FgCFPfnLl~KdoN53Cba577ogrlPTHa~Z2ZOdZhC0BK2oGv4ti74104AFd5t~PZO-ivOlRSHaYaODM-W3eXFK7-JR-e2q6g882Au9YQe1wJccTNApJpNSpV5taCfvLLUMClarraPBFBYuGY9aKhgxlciDocjYqUiODMPXkp4B-XR72O17eZ~k~HjE4IUE-jlD9d2aTb2hrG599TPCSLoiWW8jY4wKF5prLnTDRmNt3Q__',
+    imgSrc: Artem,
     imgAlt: 'Artyom Pavlushov',
     name: 'Artyom Pavlushov',
     role: 'Developer',
     bio: [
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Graduated from Bauman Moscow State Technical University. Has experience in digital marketing; as a FrontEnd Developer (JQuery); as an HTML-email layout coder. Has completed React courses in 2023',
       '',
       '<b>Contribution to the project:</b>',
       ' - CommerceTools administration',
@@ -35,13 +37,12 @@ const teamMembers = [
     github: 'https://github.com/gunsnfnr',
   },
   {
-    imgSrc:
-      'https://s3-alpha-sig.figma.com/img/23e6/0144/b7caabc004af4808d3d8a5529ce2e2b5?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pflHrXzqWTwcECxlW2VjuiiDGnAFOe2Ix7Olp1TDpoPx3iB4rx~yV6S7Dp3s0IhqS7zze74P-Xh0PpM4P48GAYW2rc~vHVzftXK29mYgC4f~iVBkqqfiIbkXqXNEkS7K1SFi27kKirWkri-ta3V3Bj~2kRYeTAakrgU2BqkCXcqK7xBrEZcgz6Ecx5cXPsfDl8RMpz83GNnjzGOh2HtwflrF-oybVmrnyzFkHyEEzx5hc73--zsbZk8L2qRJGak7Hm1g2Bskf112~0b40O~PDG3v9aynp8MDAlkpxJch6pxsUsaTk6UccUZvTXUqiqmwIjl4nRCn2jT9U8OQOQvKcg__',
+    imgSrc: Toma,
     imgAlt: 'Tamara Maltseva',
     name: 'Tamara Maltseva',
     role: 'Developer',
     bio: [
-      'I worked as a trade marketing manager for several years. I am learning java-script, and in the future I am interested in mastering a game engine or library for creating game mechanics',
+      'Graduated from St. Petersburg Technological University. Worked as a trade marketing manager for several years. Currently studying JavaScript, interested in mastering a game engine or library for creating game mechanics',
       '',
       '<b>Contribution to the project:</b>',
       ' - About Us page',
